b6c068056b49df7dc2ecbefdadca52a3f52d9534
jhumci <julian.huber@mci.edu>
2025-03-18 13:51:53 +0100

    fitlering for one zone

--- notebooks/2_exploratory_analysis/3_correlation_temp.py
+++ notebooks/2_exploratory_analysis/3_correlation_temp.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+ZONE = 1
 
 df = pd.read_csv(r"../../data/final/Load_weather_history.csv")
 df.head()
@@ -11,22 +12,26 @@ df["time"] = pd.to_datetime(df["time"])
 
 df
 
-#&& Filter only columns names load_kw and with pattern station_
-df = df.filter(regex="^(load_kw|station_.*)")
-df
+#%%
+for ZONE in df["zone_id"].unique()[0:1]:
+    df_zone = df[df["zone_id"] == ZONE]
+    # Filter only columns names load_kw and with pattern station_
+    df_zone = df_zone.filter(regex="^(load_kw|station_.*)")
+    df_zone
 
-# %%
-df_corr = df.corr()
-plt.figure(figsize=(12, 10))
-sns.heatmap(df_corr, annot=True, cmap="coolwarm")
-plt.title("Correlation Heatmap")
-plt.show()
+    df_corr = df_zone.corr()
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(df_corr, annot=True, cmap="coolwarm")
+    plt.title("Correlation Heatmap")
+    plt.show()
 
 
-# %% scatterplot matrix
+    # scatterplot matrix
 
-sns.pairplot(df)
-plt.show()
+    df = df[["load_kw", "station_1_temp_c", "station_5_temp_c"]]
+
+    sns.pairplot(df)
+    plt.show()
 
 
 # %%
